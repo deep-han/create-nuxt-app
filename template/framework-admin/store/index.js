fbbb8d38ebc3214669cd27ab0cccef6a383917bc
Han <xsytby1112@gmail.com>
2020-01-14 16:09:24 +0800

    feat: 对接 deepexi-cloud 登录

--- template/framework-admin/store/index.js
+++ template/framework-admin/store/index.js
@@ -1,9 +1,10 @@
 import cookie from 'js-cookie'
 import meta from '@/const/meta'
+import menuCode from '@/const/menu-codes'
 
 const cookieConfig = {
   get domain() {
-    const { hostname } = location
+    const {hostname} = location
     // 本地环境
     if (/^(127.0.0.1|localhost)/.test(hostname)) return hostname
     // 线上环境
@@ -16,7 +17,7 @@ const cookieConfig = {
   },
 }
 
-const createMenuItem = ({ name = '', id = '', pathUrl = '/', iconUrl }) => {
+const createMenuItem = ({name = '', id = '', pathUrl = '/', iconUrl}) => {
   return {
     url: pathUrl,
     id: id,
@@ -26,10 +27,15 @@ const createMenuItem = ({ name = '', id = '', pathUrl = '/', iconUrl }) => {
 }
 
 export const state = () => ({
+  // user info
   userId: '',
   username: '',
   avatar: '',
+
+  // meta info
   meta: meta,
+
+  // auth info
   get token() {
     return cookie.get('token', cookieConfig)
   },
@@ -39,6 +45,8 @@ export const state = () => ({
   appId: '',
   tenantId: '',
   thirdId: '',
+
+  // menu data
   headerMenu: [],
   siderMenu: [],
 })
@@ -47,9 +55,11 @@ export const getters = {
   sideMenu: state => {
     return state.siderMenu.map(createMenuItem)
   },
+
   mainMenu: state => {
     return state.headerMenu.map(createMenuItem)
   },
+
   userInfo: state => {
     return {
       name: state.username,
@@ -59,9 +69,60 @@ export const getters = {
 }
 
 export const mutations = {
+  logout(s, redirect) {
+    cookie.remove('token', cookieConfig)
+    Object.assign(s, state())
+    const loginUri = redirect
+      ? `/login?redirect=${encodeURIComponent(redirect)}`
+      : '/login'
+    this.$router.replace(loginUri)
+  },
 
+  setHeaderMenu(state, payload) {
+    state.headerMenu = payload
+  },
+
+  setUserInfo(state, payload = {}) {
+    const {params = {}, username, avatar = '', tenantId, token} = payload
+    state.username = params.nickname || username
+    state.avatar = avatar
+    state.tenantId = tenantId
+    state.token = token
+    state.userId = params.userId
+  },
 }
 
 export const actions = {
+  async login({commit, dispatch}, {body, redirect = '/'}) {
+    try {
+      const userInfo = await this.$http.login.create(body)
+      commit('setUserInfo', userInfo.payload)
+      dispatch('getHeaderMenu')
+      this.$router.replace(redirect)
 
+      return userInfo
+    } catch (error) {
+      throw error
+    }
+  },
+
+  async refresh({commit, dispatch}, token) {
+    const userInfo = await this.$http.token.index({
+      params: {
+        token,
+      },
+    })
+    dispatch('getHeaderMenu')
+    commit('setUserInfo', userInfo.payload)
+  },
+
+  async getHeaderMenu({commit}) {
+    const menus = await this.$http.menus.index({
+      params: {
+        appId: process.env.APP_ID,
+        code: menuCode.MAIN,
+      },
+    })
+    commit('setHeaderMenu', menus.payload)
+  },
 }
